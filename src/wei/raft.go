b5d1d0ecf76adae2daa967ea80563eec24a4b3b4
weiqisong <weiqisong@jd.com>
2015-06-19 17:55:11 +0800

    edit

--- src/wei/raft.go
+++ src/wei/raft.go
@@ -16,17 +16,19 @@ const (
 )
 
 type Raft struct {
-	currentTerm int32
-	rpc         RpcServer
-	events chan *CommandEvent
-	role Role
+	currentTerm   int32
+	rpc           RpcServer
+	receiveEvents chan *CommandEvent
+	sendEvents    chan *CommandEvent
+	role          Role
+	stop chan bool
+
 }
 
-type CommandEvent struct{
-	target interface{}
-	returnValue interface{}
+type CommandEvent struct {
+	target    interface{}
 	eventType CommandType
-	err error
+	err       error
 }
 
 
@@ -50,29 +52,35 @@ func (r *Raft) Start() error {
 
 
 
-}
-
-func (r *Raft) loopLeader()error{
-
 
 
 
 }
 
-func (r *Raft) loopFollower()error{
+func (r *Raft) loopLeader() error {
 
+
+
+
+}
+
+func (r *Raft) loopFollower() error {
 	t := time.NewTimer(time.Second * randomTime())
 
 	for {
 		select {
-		case event := <-r.events:
+		case event := <-r.receiveEvents:
+
+			switch event.eventType{
+			case APPEND_LOG:
+
+			}
 		case <-t.C:
 			t.Reset(time.Second*randomTime())
-			r.sendVote<-&VoteRequest{}
+			r.receiveEvents <- &CommandEvent{&VoteRequest{}, eventType:VOTE_REP}
 		}
 
 	}
-
 
 }
 func (r *Raft) loopCandidate() error {
@@ -80,20 +88,31 @@ func (r *Raft) loopCandidate() error {
 
 	for {
 		select {
-		case vote := <-r.events:
-			if r.currentTerm>vote.term{
-				r.sendVoteAck<-&VoteResponse{success:false,term:r.currentTerm}
-			} else {
-				r.sendVoteAck<-&VoteResponse{success:true,term:r.currentTerm}
-				r.role = FOLLOWER
-				return nil
+		case event := <-r.receiveEvents:
+
+			switch event.eventType{
+			case VOTE:
+				var vote VoteRequest = event.target;
+				if r.currentTerm>vote.term {
+					r.sendEvents <- &CommandEvent{target:&VoteResponse{success:false, term:r.currentTerm}, eventType:VOTE_REP}
+				} else {
+					r.receiveEvents <- &CommandEvent{&VoteResponse{success:true, term:r.currentTerm}, eventType:VOTE_REP}
+					r.role = FOLLOWER
+					return nil
+				}
 			}
 		case <-t.C:
+
+			// 如果长时间得不到应答。
 			t.Reset(time.Second*randomTime())
-			r.sendVote<-&VoteRequest{}
+			r.receiveEvents <- &CommandEvent{&VoteRequest{}, eventType:VOTE_REP}
 		}
 
 	}
+}
+
+func (r *Raft) processVote(req *VoteRequest) {
+
 }
 
 
